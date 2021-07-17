2ee61a82897958be3a81ad5ecd32a5e9a881c7ac
marciorasf <marciorasf@gmail.com>
2021-07-17 15:32:51 -0300

    add command

---
+++ behavioral/command.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+
+class SimpleCommand(Command):
+    def __init__(self, payload: str) -> None:
+        self._payload = payload
+
+    def execute(self) -> None:
+        print("SimpleCommand: See, I can do simple things like printing"
+              f"{self._payload}")
+
+
+class ComplexCommand(Command):
+    def __init__(self, receiver: Receiver, a: str, b: str) -> None:
+        self._receiver = receiver
+        self._a = a
+        self._b = b
+
+    def execute(self) -> None:
+        print("ComplexCommand: Complex studd should be done by a receiver object", end="")
+        self._receiver.do_something(self._a)
+        self._receiver.do_something_else(self._b)
+
+
+class Receiver:
+    def do_something(self, a: str) -> None:
+        print(f"\nReceiver: Working on ({a}.)", end="")
+
+    def do_something_else(self, b: str) -> None:
+        print(f"\nReceiver: Also working on ({b}.)", end="")
+
+
+class Invoker:
+    _on_start = None
+    _on_finish = None
+
+    def set_on_start(self, command: Command) -> None:
+        self._on_start = command
+
+    def set_on_finish(self, command: Command) -> None:
+        self._on_finish = command
+
+    def do_something_important(self) -> None:
+        print("Invoker: Does anybody want something done before I begin?")
+        if isinstance(self._on_start, Command):
+            self._on_start.execute()
+
+        print("Invoker: ...doing something really important ...")
+
+        print("Invoker: Does anybody want something done after I finish?")
+        if isinstance(self._on_finish, Command):
+            self._on_finish.execute()
+
+
+if __name__ == "__main__":
+    invoker = Invoker()
+    invoker.set_on_start(SimpleCommand("Say hi!"))
+    receiver = Receiver()
+    invoker.set_on_finish(ComplexCommand(
+        receiver, "Send email", "Save report"))
+
+    invoker.do_something_important()
